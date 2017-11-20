4a23997ce92cb86d013836dabe9bbcc14ece8319
Dipesh Thaker <dt36@afsconnect2.njit.edu>
2017-11-19 23:16:38 -0500

    welcome page

---
+++ welcome.php
@@ -0,0 +1,37 @@
+
+<?php
+
+
+
+class upload extends page
+{
+
+    public function post()
+    {
+        $records = accounts::findAll();
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+    public function get()
+    {
+
+
+
+
+    }
+
+
+}
+
+
